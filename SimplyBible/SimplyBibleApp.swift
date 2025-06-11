69bedaedb02568bedf411b110a097d76dccd8aba
Andrew Baughman <abaughman@brotherhoodmutual.com>
2025-06-11 16:38:12 -0400

    Initial Commit

---
+++ SimplyBible/SimplyBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyBibleApp.swift
+//  SimplyBible
+//
+//  Created by Andrew Baughman on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SimplyBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
